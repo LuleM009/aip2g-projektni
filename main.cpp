e744b5e47ec86c1387ab8320473c54683d65dcf2
lloznjak <luka.loznjak@gmail.com>
2025-06-05 06:56:11 +0000

    JNA(javno narušavanje akcijskog plana)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+
 using namespace std;
+
+const int MAX = 1000;
+
 int main()
 {
-    cout << "Hello World";
-    return 0;
-}
+    string *naziv = new string[MAX];
+    double *cijena = new double[MAX];
+    int *kolicina = new int[MAX];
+    int brojArtikala = 0;
+
+    int *kosaricaIndeksi = new int[MAX];
+    int *kosaricaKolicine = new int[MAX];
+    int brojKosarica = 0;
+
+    int brojRacuna = 1;
+    int izbor;
+
+    ifstream katalog("katalog.txt");
+    if (katalog.is_open())
+    {
+        while (brojArtikala < MAX && getline(katalog, naziv[brojArtikala]))
+        {
+            katalog >> cijena[brojArtikala];
+            katalog >> kolicina[brojArtikala];
+            katalog.ignore();
+            brojArtikala++;
+        }
+        katalog.close();
+    }
